334b03493ac39455fd731b985c6084022d803b0d
rizwanrajput <50185178+rizwanrajput@users.noreply.github.com>
2019-07-12 04:25:39 +0500

    Create click.js

---
+++ click.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+jQuery(function(){ 
+ jQuery(document).on('change', '#wpforms-96-field_15 input:radio',function() { 
+ jQuery(document).find('.step-002 .wpforms-page-next').click(); });
+});
+
+jQuery(function(){ 
+ jQuery(document).on('change', '#wpforms-96-field_9 input:radio',function() { 
+ jQuery(document).find('.step-003 .wpforms-page-next').click(); });
+});
+</script>
